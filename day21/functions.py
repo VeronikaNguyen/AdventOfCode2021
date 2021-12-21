c75fb54e7906012dc8eb37ec84309406b843772f
VeronikaNguyen <82515642+VeronikaNguyen@users.noreply.github.com>
2021-12-21 23:35:55 +0100

    Much faster solution for day 21

--- day21/functions.py
+++ day21/functions.py
@@ -1,4 +1,4 @@
-import copy
+from functools import lru_cache
 from typing import List
 
 
@@ -27,17 +27,14 @@ def play_game_with_deterministic_dice(positions: List[int]) -> int:
 
 
 def play_game_with_quantum_dice(positions: List[int]) -> int:
-    wins = simulate_quantum_dice(positions, [0, 0], [0, 0], 1, 1)
-    return wins[0] if wins[0] > wins[1] else wins[1]
+    wins1, wins2 = simulate_quantum_dice(0, 0, positions[0], positions[1], 1)
+    return wins1 if wins1 > wins2 else wins2
 
 
+@lru_cache(maxsize=1000000)
 def simulate_quantum_dice(
-    positions: List[int],
-    scores: List[int],
-    wins: List[int],
-    idx: int,
-    possibilities: int,
-) -> List[int]:
+    score1: int, score2: int, position1: int, position2: int, idx: int
+) -> (int, int):
     three_dice_roles = {
         3: 1,
         4: 3,
@@ -47,24 +44,28 @@ def simulate_quantum_dice(
         8: 3,
         9: 1,
     }
-    if scores[idx] >= 21:
-        wins[idx] += possibilities
-        return wins
-    else:
-        idx = (idx + 1) % 2
+    wins1, wins2 = 0, 0
+    if score1 >= 21:
+        return 1, 0
+    elif score2 >= 21:
+        return 0, 1
     for key in three_dice_roles.keys():
-        new_possibilities = possibilities * three_dice_roles[key]
-        new_positions = copy.deepcopy(positions)
-        new_positions[idx] = (positions[idx] + key) % 10
-        new_scores = copy.deepcopy(scores)
-        if new_positions[idx] == 0:
-            new_scores[idx] += 10
+        possibilities = three_dice_roles[key]
+        if idx == 1:
+            new_position1 = (position1 + key) % 10
+            new_score1 = score1 + 10 if new_position1 == 0 else score1 + new_position1
+            new_wins1, new_wins2 = simulate_quantum_dice(
+                new_score1, score2, new_position1, position2, 2
+            )
         else:
-            new_scores[idx] += new_positions[idx]
-        wins = simulate_quantum_dice(
-            new_positions, new_scores, wins, idx, new_possibilities
-        )
-    return wins
+            new_position2 = (position2 + key) % 10
+            new_score2 = score2 + 10 if new_position2 == 0 else score2 + new_position2
+            new_wins1, new_wins2 = simulate_quantum_dice(
+                score1, new_score2, position1, new_position2, 1
+            )
+        wins1 += possibilities * new_wins1
+        wins2 += possibilities * new_wins2
+    return wins1, wins2
 
 
 def read_from_file(filename: str) -> List[int]:
--- day21/main.py
+++ day21/main.py
@@ -1,14 +1,17 @@
+import copy
+
 from functions import (
     play_game_with_deterministic_dice,
     play_game_with_quantum_dice,
     read_from_file,
 )
 
+
 if __name__ == "__main__":
     positions = read_from_file("day21")
 
     print("Solution part 1:")
-    print(play_game_with_deterministic_dice(positions))
+    print(play_game_with_deterministic_dice(copy.deepcopy(positions)))
 
     print("\nSolution part 2:")
     print(play_game_with_quantum_dice(positions))
